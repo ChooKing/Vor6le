e02f70878da8c0cc55dc48795a047b06315f9858
ChooKing <38577527+ChooKing@users.noreply.github.com>
2022-05-24 16:57:38 -0400

    Close button added to end game

--- src/components/Finish.vue
+++ src/components/Finish.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="modal">
+        <div id="close" @click="()=>{game.ending=Endings.Playing}">
+            X
+        </div>
         <h2 v-if="game.ending==Endings.Lose">YOU LOSE</h2>
         <h2 v-if="game.ending==Endings.Win">YOU WIN</h2>
         <button @click="game.reset">Play Again</button>
     </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -16,10 +20,36 @@
         position: absolute;
         z-index: 3;
         height: 50vh;
-        width: 50vw;
+        width: 25vw;
         margin: auto;
+        padding: 0;
         font-size: 5em;
         color:rgb(45, 165, 21);
-        background-color: rgba(11, 52, 88, 0.5);
+        background-color: rgba(11, 52, 88, 0.85);
+    }
+    .modal button{
+        background-color: rgb(79, 129, 25);
+        font-size: 4rem;
+        cursor: pointer;
+    }
+    button:hover{
+        background-color: rgb(11, 80, 57);
+        color: yellow;
+        
+    }
+    #close{
+        position: absolute;
+        margin: 5px;
+        padding: 5px;
+        top: 0;
+        right: 0;    
+        border: 1px solid rgb(16, 1, 1);    
+        font-size: 2rem;
+        background-color: rgb(40, 40, 168);
+        color: black;
+        cursor:pointer;
+    }
+    #close:hover{
+        background-color:rgb(18, 155, 109)
     }
 </style>
